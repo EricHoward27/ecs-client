5accd6098d181f49ea5b81f0f41ffef1c8537858
eric <erichoward2141@gmail.com>
2021-05-02 16:13:11 -0500

    add images to about components, update text in the component for marketing of web app

--- src/App.js
+++ src/App.js
@@ -28,13 +28,25 @@ function App() {
       link: 'https://erichoward27.github.io/survey-client/'
     }
   ]
+  const aboutLink = [
+    {
+      year: '2021',
+      link: 'ECSLOGO.png',
+      heading: 'ECS first open as a Studio',
+      body: 'A full stack developer, Eric Howard, decided to open ECS studio, after having many years of ambition with creating software. With technology advancing, more and more people are getting their hands on many devices. With his passion of technology, Eric always believe he could help push the industry further, and he enjoys bringing business owners with a vision to the net.',
+      invert_link: 'ecs.png',
+      invert_heading: 'Our Journey',
+      invert_year: '2021-beyond',
+      invert_body: 'ECS would love for you to be apart of our journey. ECS have a vision to expand and provide many businesses with professional web applications. Join us on this epic journey!'
+    }
+  ]
   return (
     <div className="App">
      <Header />
      <Masthead />
      <Services />
      <Portfolio portfolioLinks={portfolioLinks}/>
-     <About />
+     <About aboutLink={aboutLink}/>
      <Team />
      <Contact />
      <Footer />
--- src/components/About.js
+++ src/components/About.js
@@ -1,15 +1,38 @@
 import React from 'react'
 
-const About = () => {
+const About = ({ aboutLink }) => {
   return (
     <section className="page-section" id="about">
         <div className="container">
             <div className="text-center">
-                <h2 className="section-heading text-uppercase">About</h2>
-                <h3 className="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+                <h2 className="section-heading text-uppercase">About Our Studio</h2>
+                <h3 className="section-subheading text-muted">Learn more about ECS.</h3>
             </div>
-            <ul className="timeline">
-                <li>
+              {  aboutLink && aboutLink.map(({ year, heading, body, link, invert_year, invert_heading, invert_body, invert_link }, index) =>
+                <ul className="timeline">
+                  <li>
+                        <div className="timeline-image"><img className="rounded-circle img-fluid" src={ link } alt="..." /></div>
+                        <div className="timeline-panel">
+                            <div className="timeline-heading">
+                                <h4>{ year }</h4>
+                                <h4 className="subheading">{ heading }</h4>
+                            </div>
+                            <div className="timeline-body"><p className="text-muted">{ body }</p></div>
+                        </div>
+                    </li>
+                    <li className="timeline-inverted">
+                        <div className="timeline-image"><img className="rounded-circle img-fluid" src={ invert_link } alt="..." /></div>
+                        <div className="timeline-panel">
+                            <div className="timeline-heading">
+                                <h4>{ invert_year }</h4>
+                                <h4 className="subheading">{ invert_heading }</h4>
+                            </div>
+                            <div className="timeline-body"><p className="text-muted">{ invert_body }</p></div>
+                        </div>
+                    </li>
+                </ul>
+              )}
+                {/* <li>
                     <div className="timeline-image"><img className="rounded-circle img-fluid" src="assets/img/about/1.jpg" alt="..." /></div>
                     <div className="timeline-panel">
                         <div className="timeline-heading">
@@ -60,7 +83,7 @@ const About = () => {
                         </h4>
                     </div>
                 </li>
-            </ul>
+            </ul> */}
         </div>
     </section>
   )
